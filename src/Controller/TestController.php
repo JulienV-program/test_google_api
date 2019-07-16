a8bad1c1c44b84df68d3ef585c28f97a5144dad2
JulienV-program <julien.valliorgue@free.fr>
2019-07-16 14:51:37 +0200

    initial commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Google_Client;
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/", name="test")
+     */
+    public function index()
+    {
+        $client = new Google_Client();
+        $client->setApplicationName('Google Calendar API PHP Quickstart');
+        $client->setScopes(\Google_Service_Calendar::CALENDAR_READONLY);
+        $client->setAuthConfig('credentials.json');
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = 'token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        // Get the API client and construct the service object.
+//        $client = getClient();
+        $service = new \Google_Service_Calendar($client);
+
+// Print the next 10 events on the user's calendar.
+        $calendarId = 'primary';
+        $optParams = array(
+            'maxResults' => 10,
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => date('c'),
+        );
+        $results = $service->events->listEvents($calendarId, $optParams);
+        $events = $results->getItems();
+
+        if (empty($events)) {
+            print "No upcoming events found.\n";
+        } else {
+            print "Upcoming events:\n";
+            foreach ($events as $event) {
+                $start = $event->start->dateTime;
+                $end = $event->end->dateTime;
+                var_dump($event);
+                if (empty($start)) {
+                    $start = $event->start->date;
+                }
+//        echo "début : ";
+                printf("%s (%s) (%s)\n", $event->getSummary(), $start, $end);
+
+            }
+            dump($event);
+            return $this->render('test/index.html.twig', [
+                'controller_name' => 'TestController',
+            ]);
+        }
+    }
+}
